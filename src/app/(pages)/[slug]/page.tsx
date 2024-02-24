f4ef45bee317be8b17c0f59a0c4fa1e1e4852136
Rushish Mewada <mewadarushish@gmail.com>
2024-02-24 11:26:35 +0530

    Done

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -65,7 +65,6 @@ export default async function Page({ params: { slug = 'home' } }) {
         <section>
 
           <Hero {...hero} />
-
           <HomePage />
           <Gutter className={classes.home}>
             <Categories categories={categories} />
